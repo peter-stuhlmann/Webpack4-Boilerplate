c2af6d062f9126742299308cbbe177306f1bf8f8
Peter R. Stuhlmann <info@peter-stuhlmann.de>
2019-03-11 17:24:43 +0100

    Added css/scss extract rules

--- webpack.config.js
+++ webpack.config.js
@@ -22,6 +22,37 @@ module.exports = {
                 test: /\.css$/,
                 use: ['style-loader', 'css-loader']
             },
+            {
+                test: [/.css$|.scss$/],
+                use: [
+                    MiniCssExtractPlugin.loader,
+                    {
+                        loader: 'css-loader',
+                        options: {
+                            sourceMap: true
+                        }
+                    },
+                    {
+                        loader: 'postcss-loader',
+                        options: {
+                            ident: 'postcss',
+                            sourceMap: true,
+                            plugins: [
+                                require('autoprefixer')({
+                                    browsers: ['> 1%', 'last 2 versions']
+                                })
+                            ]
+                        }
+                    },
+                    {
+                        loader: 'sass-loader',
+                        options: {
+                            sourceMap: true
+                        }
+                    }
+        ]
+      },
+
         ]
     }
 }
