81c3da0835efe223851915ad242a9270910405ef
Peter R. Stuhlmann <info@peter-stuhlmann.de>
2019-03-11 17:44:45 +0100

    Added devtools

--- webpack.config.js
+++ webpack.config.js
@@ -10,6 +10,7 @@ module.exports = {
         path: path.resolve(__dirname, 'dist/'),
         filename: 'assets/js/main.js'
     },
+    devtool: "source-map",
     module: {
         rules: [
             {
